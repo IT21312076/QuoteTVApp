a879bdbc2b642016a81c4e4c61b93e2c6b27f32a
anuththara <it21232640@my.sliit.lk>
2024-11-27 22:03:47 +0530

    Initial Commit

---
+++ QuoteTVApp/QuoteTVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteTVAppApp.swift
+//  QuoteTVApp
+//
+//  Created by Anuththara Divarathna on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct QuoteTVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
